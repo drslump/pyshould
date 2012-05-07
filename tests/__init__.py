87a3fca5f412b82d90246b269a95163373dcd95d
DrSlump <drslump@pollinimini.net>
2012-05-07 21:02:29 +0200

    Added coordination unit tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,11 @@
 import unittest
 
 from dsl import DslTestCase
+from coordination import CoordinationTestCase
 
 
 def all_tests():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(DslTestCase))
+    suite.addTest(unittest.makeSuite(CoordinationTestCase))
     return suite
