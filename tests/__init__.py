2bd3ad4814a9e25321bbae45a78dbbfc350f5214
DrSlump <drslump@pollinimini.net>
2012-05-08 13:23:59 +0200

    Unit tests for the expect api

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,10 +2,11 @@ import unittest
 
 from dsl import DslTestCase
 from coordination import CoordinationTestCase
-
+from expect import ExpectTestCase
 
 def all_tests():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(DslTestCase))
     suite.addTest(unittest.makeSuite(CoordinationTestCase))
+    suite.addTest(unittest.makeSuite(ExpectTestCase))
     return suite
---
+++ tests/expect.py
@@ -0,0 +1,22 @@
+import unittest
+from pyshould.expect import expect, expect_all, expect_any, expect_none
+
+class ExpectTestCase(unittest.TestCase):
+    """ Simple tests for the expect based api """
+
+    def test_expect(self):
+        expect(1).to_equal(1)
+        expect(1).to_not_equal(0)
+
+    def test_expect_all(self):
+        expect_all([1,2]).to_be_integer()
+        expect_all(1, 2).to_be_integer()
+
+    def test_expect_any(self):
+        expect_any([1,2]).to_equal(2)
+        expect_any(1,2).to_equal(2)
+
+    def test_expect_none(self):
+        expect_none([1,2]).to_equal(0)
+        expect_none(1,2).to_equal(0)
+
