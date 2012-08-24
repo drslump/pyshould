892f5f33b0df05780bcaa95860d216a946ee8723
DrSlump <drslump@pollinimini.net>
2012-08-24 17:09:33 +0200

    Renamed examples file to avoid being pick up by the test runner

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyshould',
+    description='Should style asserts based on pyhamcrest',
     version='0.1.0',
     url='https://github.com/drslump/pyshould',
     author='Ivan -DrSlump- Montes',
     author_email='drslump@pollinimini.net',
-    packages = find_packages(),
-    include_package_data = True,
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=['pyhamcrest'],
-    zip_safe = False,
+    zip_safe=False,
     )
