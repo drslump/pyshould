0b35ba283b86553f318b11dcc73faf52b0eacf49
DrSlump <drslump@pollinimini.net>
2012-09-21 13:19:30 +0200

    Upped the version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pyshould',
     description='Should style asserts based on pyhamcrest',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/drslump/pyshould',
     author='Ivan -DrSlump- Montes',
     author_email='drslump@pollinimini.net',
