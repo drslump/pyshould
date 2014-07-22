3e91fad49c44ce262035b8066268ae9a22700a2a
DrSlump <drslump@pollinimini.net>
2014-07-22 15:54:25 +0200

    Tagging version 0.6.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pyshould',
     description='Should style asserts based on pyhamcrest',
-    version='{VERSION}',
+    version='0.6.0',
     url='https://github.com/drslump/pyshould',
     author='Ivan -DrSlump- Montes',
     author_email='drslump@pollinimini.net',
